3417e6019f5a700c9ceb9f0bc97931361ea78358
Erick Malta <erickmalta100@gmail.com>
2020-12-02 22:47:45 -0300

    fix(config): :wrench: fix typeorm entities expression

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Task } from '../tasks/task.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -7,6 +8,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: 'nest123',
   database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity.{js|ts}'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
